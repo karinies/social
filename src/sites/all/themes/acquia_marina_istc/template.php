3f5d36686790f44206190f7204c6e4660d5bd26c
karnies <kari@ics.uci.edu>
2013-02-11 15:02:38 -0800

    Changed the default file link to be "download" unless a 'description' is given.  Filenames must now be entered manually under the description if to appear as link.
    Must turn off 'description' option for file_pub so that publications always appear as "download".

---
+++ src/sites/all/themes/acquia_marina_istc/template.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function acquia_marina_istc_file_link($variables) {
+  $file = $variables['file'];
+  $icon_directory = $variables['icon_directory'];
+
+  $url = file_create_url($file->uri);
+  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
+
+  // Set options as per anchor format described at
+  // http://microformats.org/wiki/file-format-examples
+  $options = array(
+    'attributes' => array(
+      'type' => $file->filemime . '; length=' . $file->filesize,
+    ),
+  );
+
+  // Use the description as the link text if available.
+  if (empty($file->description)) {
+    $link_text = "download"; /*$file->filename;*/
+  }
+  else {
+    $link_text = $file->description;
+    $options['attributes']['title'] = check_plain($file->filename);
+  }
+  //dpm($variables);
+  return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
+}
+
+ 
+
+?>
